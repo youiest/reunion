8222de111380a6e21e5215fdfbb42e2a2df302f1
wiber <elias@youiest.com>
2015-07-17 12:36:28 +0200

    copied

---
+++ client/lib/app.browserify.js
@@ -0,0 +1,5 @@
+mui = require('material-ui');
+injectTapEventPlugin = require('react-tap-event-plugin');
+let Colors = require('../colors');
+let Spacing = require('../spacing');
+let ColorManipulator = require('../../utils/color-manipulator');
